f2424ba93fcf311df361e513875c2685df8e16f5
Peter G <petergmarco@gmail.com>
2020-06-17 14:45:06 -0400

    added pool custom post type, added particles for contact page and pool pages, added page templates for pool model pages

--- functions.php
+++ functions.php
@@ -1,3 +1,105 @@
 <?php
 
+add_filter( 'wpcf7_form_elements', 'sc_wpcf7_form_elements' );
+ 
+function sc_wpcf7_form_elements( $form ) {
+		$form = do_shortcode( $form );
+		return $form;
+}
 
+add_action('init', 'aquamed_pool_types_init');
+
+function aquamed_pool_types_init(){
+
+	$labels = array(
+		'name'  		  	 => _x('Pools', 'post type name', 'g5_aquapools'),
+		'singular_name' 	 => _x('Pool', 'post type singular name', 'g5_aquapools'),
+		'menu_name'			 => _x('Pools', 'admin menu', 'g5_aquapools'),
+		'name_admin_bar'  	 => _x('Pool', 'add new on admin bar', 'g5_aquapools'),
+		'add_new' 			 => _x('Add New', 'Add New Pool', 'g5_aquapools'),
+		'add_new_item'   	 => _x('Add New Pool', 'g5_aquapools'),
+		'new_item'		 	 => _x('New Pool', 'g5_aquapools'),
+		'edit_item'		 	 => _x('Edit Pool', 'g5_aquapools'),
+		'view_item'			 => _x('View Pool', 'g5_aquapools'),
+		'all_items' 		 => _x('All Pools', 'g5_aquapools'),
+		'search_items' 		 => _x('Search pools', 'g5_aquapools'),
+		'not_found'			 => _x('No pools found', 'g5_aquapools'),
+		'not_found_in_trash' => _x('No pools found in trash', 'g5_aquapools')
+	);
+
+	$args = array(
+		'labels' 			=> $labels,
+		'description'		=> __('Description.', 'g5_aquapools'),
+		'public'			=> true,
+		'public_queryable'	=> true,
+		'show_ui'			=> true,
+		'show_in_menu'		=> true,
+		'query_var'			=> true,
+		'rewrite'			=> array('slug' => 'pool'),
+		'capability_type'	=> 'post',
+		'has_archive'		=> false,
+		'menu_position'		=> null,
+		'supports'			=> array('title', 'editor', 'author', 'thumbnail', 'excerpt')
+	);
+
+	register_post_type('pool', $args);
+
+	$args = array(
+			'hierarchical'			=> true,
+			'labels'				=> 'Models',
+			'singular_label' 		=> 'Model',
+			'query_var'				=> true,
+			'rewrite'				=> true,
+			'slug'	 				=> 'pool-model',
+	);
+	register_taxonomy('pool-model', 'pool', $args );
+
+}
+
+add_filter('manage_edit-pool_columns', "pool_manager_edit_columns");
+
+add_action('after_setup_theme', 'aquamed_image_sizes');
+
+function aquamed_image_sizes(){
+	add_image_size('large-thumbnails', 370, 225, true);
+}
+
+function pool_manager_edit_columns($columns){
+	$columns = array(
+		"cb" 			=> "<input type=\"checkbox\" />",
+		"title"			=> "Pool Name",
+		"created"		=> 'Created Date',
+		"cat"			=> "Model",
+		);
+	return $columns;
+}
+
+add_action("manage_pool_posts_custom_column", "pool_manager_custom_columns");
+
+function pool_manager_custom_columns($column){
+	global $post;
+	$output = get_post_custom();
+
+	switch($column)
+	{
+		case "created":
+			$created = get_the_date();
+			echo $created;
+			break;
+		case "cat":
+			$type = get_the_term_list($post->ID, 'pool-model');
+			echo $type;
+			break;
+	}
+}
+
+add_filter('manage_edit-pool_sortable_columns', 'pool_sortable_columns');
+
+function pool_sortable_columns( $columns ){
+
+	$columns['created'] = 'created';
+	$columns['cat'] = 'cat';
+	return $columns;
+}
+
+?>
---
+++ page-thursday-pools.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @package   Gantry 5 Theme
+ * @author    RocketTheme http://www.rockettheme.com
+ * @copyright Copyright (C) 2007 - 2017 RocketTheme, LLC
+ * @license   GNU/GPLv2 and later
+ *
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+defined('ABSPATH') or die;
+
+use Timber\Timber;
+
+/*
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * To generate specific templates for your pages you can use:
+ * /mytheme/views/page-mypage.html.twig
+ * (which will still route through this PHP file)
+ * OR
+ * /mytheme/page-mypage.php
+ * (in which case you'll want to duplicate this file and save to the above path)
+ */
+
+$gantry = Gantry\Framework\Gantry::instance();
+$theme  = $gantry['theme'];
+
+// We need to render contents of <head> before plugin content gets added.
+$context              = Timber::get_context();
+$context['page_head'] = $theme->render('partials/page_head.html.twig', $context);
+
+$args = array(
+	    'post_type' => 'pool',
+	    'posts_per_page' => -1,
+	    'tax_query' => array(
+	    	'relation' => 'AND',
+	        array(
+	            'taxonomy' => 'pool-model',
+	            'field' => 'slug',
+	            'terms' => array('thursday-pools')
+	        )
+	    ),
+	    'orderby' => array(
+	    	'date' => 'ASC'
+));
+
+$post            = Timber::get_posts( $args );
+$context['pools'] = $post;
+
+Timber::render('page-thursday-pools.html.twig', $context);
+
